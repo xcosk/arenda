4f2a002f2d8273d9c58cd4c8bd429e33e2ed3d79
maksim <maks.m90.00@mail.ru>
2024-12-08 22:56:30 +0300

    dd

--- src/routes/Root/Root.jsx
+++ src/routes/Root/Root.jsx
@@ -1,16 +1,23 @@
 import { NavLink, Outlet } from "react-router-dom"
+import s from "./Root.module.css"
 
 export const Root = () => {
     return (
-        <>
-            <nav>
-                <NavLink to={"Os"}>Premium</NavLink>
-                <NavLink to={"Oss"}>Личный кабинет</NavLink>
-                <NavLink></NavLink>
+        <div className={s.div}>
+            <nav className={s.nav}>
+                <NavLink className={s.a} to={"Os"}>Premium</NavLink>
+                <NavLink className={s.b} to={"Oss"}>Личный кабинет</NavLink>
+                <div className={s.navDiv}>
+                    <NavLink><img src="/images/whatsapp_2.png (1).svg" alt="" /></NavLink>
+                    <NavLink><img src="/images/instagram_12.png.svg" alt="" /></NavLink>
+                    <NavLink><img src="/images/telegram.png (2).svg" alt="" /></NavLink>
+                    <NavLink><button className={s.sv}>Связаться с нами</button></NavLink>
+                </div>
             </nav>
             <div>
                 <Outlet />
             </div>
-        </>
+
+        </div>
     )
 }
